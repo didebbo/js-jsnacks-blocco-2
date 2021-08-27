3263bfe71f19c2c72657c77978b535c69901b0bc
didebbo <gianluca.napoletano.93@gmail.com>
2021-08-27 12:56:15 +0200

    snack-3

---
+++ snack-3/index.js
@@ -0,0 +1,22 @@
+// Inserisci un numero, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+
+const nomi = ["Angelo", "Bruno", "Carlo", "Davide"];
+const cognomi = ["Empoli", "Fuffa", "Girondi", "Hat"];
+let invitati = [];
+const quantiInvitati = 3;
+let log = "Gli invitati sono:";
+
+for (let i = 0; i < quantiInvitati; i++) {
+    let nome = nomi[Math.floor(Math.random() * nomi.length)];
+    let cognome = cognomi[Math.floor(Math.random() * cognomi.length)];
+    invitati.push(nome + " " + cognome);
+}
+
+invitati.forEach((invitato) => {
+    log += " " + invitato;
+})
+
+alert(log);
+
+
